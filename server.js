5b0ddfeaf2853d7c5b9441190fab48a75902a55c
Michael Pacheco <michaelpacheco@Michaels-MacBook-Pro.local>
2017-09-22 13:37:04 -0700

    server created

---
+++ server.js
@@ -0,0 +1,50 @@
+// DEPENDENCIES
+var express = require("express");
+var bodyParser = require("body-parser");
+var routes = require("./routes/routes");
+var socket = require("socket.io");
+var cookieParser = require('cookie-parser');
+
+// Setting up Express app
+// =====================================
+var app = express();
+var PORT = process.env.PORT || 8083;
+
+// Sets up server for sockets
+var server = require('http').Server(app);
+var io = socket(server);
+
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+// app.use(methodOverride('_method'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+
+    // PUT ROUTES IN HERE
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({force:true}).then(function() {
+    /*  app.listen(PORT, function() {
+            console.log("App listening on PORT " + PORT);
+          });*/
+        server.listen(PORT, function() {
+            console.log('Server listening on PORT: ', PORT);
+        });
+    });
+
+// SOCKET IO CONNECTIONS
+// ==================================================
+
+  // SOCKET STUFF IN HERE
